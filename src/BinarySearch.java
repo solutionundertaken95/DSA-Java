98894f25fabaa19ea5d0c35515b6e339aef3e7c4
Gokul S Ganesh <gokulsg02@gmail.com>
2026-02-14 19:50:14 +0530

    Added Binary Search

---
+++ src/BinarySearch.java
@@ -0,0 +1,46 @@
+package src;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class BinarySearch {
+
+    public static int binarySearch(int arr[], int target){
+
+        int left=0;
+        int right = arr.length-1;
+
+
+        while(left < right){
+
+            int mid = (left + right)/2;
+
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if(arr[mid]<target){
+                left = mid +1 ;
+            }
+            else{
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+
+        int[] num = {2,4,6,8,10};
+        int target = 5;
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!\n");
+        System.out.printf("Binary Search\n");
+
+        int index = binarySearch(num,target);
+        System.out.println(index);
+        if(index == -1){
+            System.out.println("Target not present in the list");
+        }
+        else
+            System.out.println("Target present at position : " +index);
+    }
+}
