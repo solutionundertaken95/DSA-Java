c08073a49127dec655b46aa618e371c5e208d471
Gokul S Ganesh <gokulsg02@gmail.com>
2026-02-25 17:56:25 +0530

    Modified quick sort

--- src/Sorting/QuickSort.java
+++ src/Sorting/QuickSort.java
@@ -26,33 +26,31 @@ public class QuickSort {
 
     private static void quicksort(int[] nums, int low, int high) {
 
-        int pi = partition(nums,low,high);
-
-        quicksort(nums, low, pi-1);
-        quicksort(nums,pi + 1, high);
-
-
+        if (low < high) {
+            int pi = partition(nums, low, high);
+            quicksort(nums, low, pi - 1);
+            quicksort(nums, pi + 1, high);
+        }
     }
 
     private static int partition(int[] nums, int low, int high) {
-         int pivot = nums[high];
-         int i = low-1;
 
-         for(int j = low;j< nums.length;j++){
-              if(nums[j]< pivot){
-                  i++;
-                  int temp = nums[i];
-                  nums[i] = nums[j];
-                  nums[j] = temp;
-              }
-         }
+        int pivot = nums[high];
+        int i = low - 1;
 
-         int temp = nums[i+1];
-         nums[i+1] = pivot;
-         pivot = temp;
+        for (int j = low; j < high; j++) {
+            if (nums[j] < pivot) {
+                i++;
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
 
+        int temp = nums[i + 1];
+        nums[i + 1] = nums[high];
+        nums[high] = temp;
 
-         return i+1;
-
+        return i + 1;
     }
 }
