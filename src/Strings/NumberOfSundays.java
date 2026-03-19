11af763c61f2a932d8cb5f4616c79da49a2484fd
Gokul S Ganesh <gokulsg02@gmail.com>
2026-03-19 12:55:48 +0530

    Number of Sundays Problem

---
+++ src/Strings/NumberOfSundays.java
@@ -0,0 +1,46 @@
+package src.Strings;
+
+import java.util.Scanner;
+
+public class NumberOfSundays {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the starting Day");
+        String mon = scanner.nextLine();
+
+        System.out.println("Enter the number of days from the start day");
+        int n = scanner.nextInt();
+
+        String [] arr = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        boolean found =false;
+        int i=0;
+
+        for(i=0;i< arr.length;i++){
+            if(mon.equals(arr[i])){
+                found = true;
+                break;
+            }
+        }
+
+        if(!found){
+            System.out.println("Invalid Input Month");
+            return;
+        }
+
+
+        int count =0;
+        int rem = 6-i;
+
+        if(n>=rem){
+            count = 1;
+            n = n - rem;
+
+            count+=n/7;
+        }
+
+        System.out.println("The Number of Sundays are : " + count);
+    }
+}
